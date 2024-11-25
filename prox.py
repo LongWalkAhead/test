bdf17467ba05d19ab5bbd70f6f2405d15ef8bfad
LongWalkAhead <bluelama1234@gmail.com>
2024-11-24 19:36:35 -0500

    Add files via upload

---
+++ prox.py
@@ -0,0 +1,9 @@
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+print(grades)
+amount = 10
+average_number = 85 + 90 + 78 + 88 + 76 + 95 + 89 + 80 + 72 + 93
+print(average_number)
+total =average_number // amount 
+print(total)
+
---
+++ prox2.py
@@ -0,0 +1,4 @@
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"] 
+if "Alice" in attended and submitted:
+    print ("she is all caught up!") 
